a21c0faef55b85f6afd88fc53fdb4050c66f4dc6
Pavol Novotny <pavol.novotny@drmax.cz>
2022-08-29 09:50:20 +0200

    footer

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,137 +1,94 @@
 <template>
-  <div>
-    <section class="contact-area" id="contact">
-      <cookie-law theme="dark-lime"></cookie-law>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6 offset-lg-3">
-            <div class="contact-content text-center">
-              <a href="#"><img src="https://i.ibb.co/QDy827D/ak-logo.png" alt="logo"></a>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum </p>
-              <div class="hr"></div>
-              <h6>1120 Lorem ipsum dolor sit amet, KC 179050, Chandigarh.</h6>
-              <h6>+01 2345 6789 12<span>|</span>+01 2345 6789 12</h6>
-              <div class="contact-social">
-                <ul>
-                  <li><a class="hover-target" href=""><i class="fab fa-facebook-f"></i></a></li>
-                  <li><a class="hover-target" href=""><i class="fab fa-linkedin-in"></i></a></li>
-                  <li><a class="hover-target" href=""><i class="fab fa-github"></i></a></li>
-                  <li><a class="hover-target" href=""><i class="fab fa-behance"></i></a></li>
-                  <li><a class="hover-target" href=""><i class="fab fa-pinterest-p"></i></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
+  <footer>
+    <b-container>
+      <b-row>
+        <ul class="d-flex align-items-center justify-content-center mt-4">
+          <li class="d-inline-block">
+            <a href="https://www.facebook.com/smiga.michal" target="_blank"><BIconFacebook class="facebook"/></a>
+            </li>
+          <li class="d-inline-block ml-3 mr-3 ">
+            <a href="https://www.instagram.com/smiga.michal/" target="_blank"><BIconInstagram class="instagram"/></a>
+          </li>
+          <li class="d-inline-block">
+            <a href="https://www.linkedin.com/in/smigamichal/?locale=cs_CZ" target="_blank"><BIconLinkedin class="linkedin"/></a>
+          </li>
+        </ul>
+      </b-row>
+      <b-row>
+        <div class="d-flex align-items-center justify-content-center mt-2">
+          <img class="logo" src="~/static/images/logo.svg"/>
+          <ul class="d-flex flex-column position-relative">
+            <li class="d-flex">
+              <BIconGeoAlt class="icon align-self-center"/>
+              <div><p class="mb-0">Kaprova 52/6 Praha 1</p><p class="mb-0">Křižíkova 506/62 Praha 8</p></div>
+            </li>
+            <li class="d-inline-block"><BIconTelephone class="icon"/><span>725 747 519</span></li>
+            <li class="d-inline-block"><BIconEnvelope class="icon"/><span>michal.smiga@kwcz.cz</span></li>
+          </ul>
         </div>
-      </div>
-    </section>
-    <!-- =============== 1.9 Contact Area End ====================-->
-    <!-- =============== 1.9 Footer Area Start ====================-->
-    <footer>
-      <p>Copyright &copy; 2019 All Rights Reserved.</p>
-    </footer>
-  </div>
+      </b-row>
+    </b-container>
+    <hr class="mb-1 mt-1"/>
+    <b-container>
+      <b-row>
+        <p class="text-center"> &copy; <span>{{new Date().getFullYear()}}</span> {{ $t('footer') }} </p>
+      </b-row>
+    </b-container>
+  </footer>
 </template>
 
 <script>
+import { BIconFacebook, BIconInstagram, BIconLinkedin, BIconGeoAlt, BIconTelephone, BIconEnvelope } from 'bootstrap-vue'
 export default {
-  name: 'BVFooter',
-
-  props: {
-    isDocs: {
-      type: Boolean,
-      default: false
-    }
-  },
-
-  computed: {
-
+  name: 'footer-bottom',
+  components: {
+    BIconFacebook,
+    BIconInstagram,
+    BIconLinkedin,
+    BIconGeoAlt,
+    BIconTelephone,
+    BIconEnvelope
   }
 }
 </script>
 
 <style scoped>
-img {
-  max-width: 100%;
-  height: auto;
+footer {
+  background-color: #41B3A3;
+  border-top: 1px solid black;
 }
-section {
-  padding: 60px 0;
-  /* min-height: 100vh;*/
-}
-ul {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-.contact-area {
-  border-bottom: 1px solid #353C46;
-}
-
-.contact-content p {
-  font-size: 15px;
-  margin: 30px 0 60px;
-  position: relative;
-}
-
-.contact-content p::after {
-  background: #353C46;
-  bottom: -30px;
-  content: "";
-  height: 1px;
-  left: 50%;
-  position: absolute;
-  transform: translate(-50%);
-  width: 80%;
-}
-
-.contact-content h6 {
-  color: #8b9199;
-  font-size: 15px;
-  font-weight: 400;
-  margin-bottom: 10px;
-}
-
-.contact-content span {
-  color: #353c47;
-  margin: 0 10px;
-}
-
-.contact-social {
-  margin-top: 30px;
-}
-
-.contact-social > ul {
-  display: inline-flex;
-}
-
-.contact-social ul li a {
-  border: 1px solid #8b9199;
-  color: #8b9199;
+.logo {
+  width: 100px;
+  height: 60px;
   display: inline-block;
-  height: 40px;
-  margin: 0 10px;
-  padding-top: 7px;
-  transition: all 0.4s ease 0s;
-  width: 40px;
+}
+.icon {
+  margin-right: 8px;
+  width: 20px;
+  height: 20px;
+  font-weight: 900;
+}
+.facebook, .instagram, .linkedin {
+  display: inline-block;
+  height: 45px;
+  width: 45px;
+  transition: all .2s ease-in-out;
+  background-color: white;
+  margin: 5px;
+  border-radius: 50%;
 }
 
-.contact-social ul li a:hover {
-  border: 1px solid #FAB702;
-  color: #FAB702;
+.facebook:hover,.instagram:hover,.linkedin:hover {
+  transform: scale(1.5);
 }
 
-.contact-content img {
-  max-width: 210px;
+.facebook {
+  color: #4267B2;
 }
-
-section, footer {
-  background: #1A1E25;
-  color: #868c96;
+.instagram {
+  color: #C13584;
 }
-
-footer p {
-  padding: 40px 0;
-  text-align: center;
+.linkedin {
+  color: #0A66C2;
 }
 </style>
